4b9ef4757aa78380585bde9fab62fd85e09eccab
unknown <kim.pearton@live.co.uk>
2022-04-21 13:46:47 +0100

    Add in routes for log in, log out, register

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import os
 app = Flask(__name__)
 app.secret_key = 'some secret'
 
+cluster = MongoClient('mongodb+srv://admin:N4yqaCy7BTzZe8sw@resumegeneratorcluster.0spsy.mongodb.net/resume-generator')
+db = cluster['resume-generator']
+users = db['users']
 
 @app.route('/')
 
@@ -13,6 +16,49 @@ app.secret_key = 'some secret'
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return render_template('index.html',
+                                message='You are already logged in!')
+    if request.method == 'POST':
+        users = db['users']
+        user_login = users.find_one({'username': request.form['username']})
+        if user_login:
+            if request.form['password'] == user_login['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+        return render_template('login.html',
+                                    message='Invalid username or password')
+    return render_template('login.html', message='')
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+        return render_template('message.html',
+                                message='Logged out. See you later!')
+    return render_template('message.html',
+                            message='You have already logged out!')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if 'username' in session:
+        return render_template('register.html',
+                                message='You are already logged in and registered')
+    if request.method== 'POST':
+        users = db['users']
+        existing_user = users.find_one({'username': request.form['username']})
+        if request.form['username'] and request.form['password']:
+            if existing_user is None:
+                password = request.form['password']
+                users.insert_one({'username': request.form['username'],
+                                'password': password})
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            return render_template('register.html', message='Username ' + str(existing_user['username']) + ' already exists')
+        return render_template('register.html', message='Enter a username and password')
+    return render_template('register.html', message='')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
