fddc911485584c78b18581ef6818cc34b4a47e1a
unknown <kim.pearton@live.co.uk>
2022-04-21 11:45:48 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, request, url_for, session
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+app = Flask(__name__)
+app.secret_key = 'some secret'
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT', 5000)),
+    debug=True)
